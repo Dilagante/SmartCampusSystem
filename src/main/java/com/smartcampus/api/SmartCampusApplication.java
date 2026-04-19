8289622e987cebe9cebd9b9ee76643e77e38f517
Dilagante <dinethdds@gmail.com>
2026-04-19 16:34:49 +0530

    chore: initial commit

---
+++ src/main/java/com/smartcampus/api/SmartCampusApplication.java
@@ -0,0 +1,11 @@
+package com.smartcampus.api;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+
+@ApplicationPath("/api/v1/")
+public class SmartCampusApplication extends Application {
+
+    /* TODO */
+}
